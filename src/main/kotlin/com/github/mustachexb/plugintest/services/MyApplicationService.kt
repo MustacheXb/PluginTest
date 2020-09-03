e06363c851ff23410d02ad466dfde9ee6d0d6425
GitHub Action <action@github.com>
2020-09-03 01:37:52 +0000

    Template cleanup

--- src/main/kotlin/com/github/mustachexb/plugintest/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/mustachexb/plugintest/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.mustachexb.plugintest.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mustachexb.plugintest.MyBundle
 
 class MyApplicationService {
 
--- src/main/kotlin/com/github/mustachexb/plugintest/services/MyProjectService.kt
+++ src/main/kotlin/com/github/mustachexb/plugintest/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.mustachexb.plugintest.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.mustachexb.plugintest.MyBundle
 
 class MyProjectService(project: Project) {
 
